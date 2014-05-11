3769b155fa19cc8cdd92df6f298e307835fb8630
jackbeckwith <jackbeckwith@gmail.com>
2014-05-11 13:41:52 -0400

    Test

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,8 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 100}
   validates :genre, presence: true
   has_attached_file :audio,
+  :url => "/system/:class/:attachment/:id/:style/:basename.:extension",
+  :path => "http://thawing-temple-3343.herokuapp.com/public/system/:class/:attachment/:id/:style/:basename.:extension"
 
   validates_attachment_presence :audio
   validates_attachment_content_type :audio, :content_type => ['application/mp3', 'application/x-mp3', 'audio/mpeg', 'audio/mp3'],
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,15 +87,13 @@ MySite::Application.configure do
     :domain => ENV['SENDGRID_DOMAIN']
   }
 
-  config.paperclip_defaults = {
-          :storage =&gt; :s3,
-          :s3_credentials =&gt; {
-            :bucket =&gt; ENV['AWS_BUCKET'],
-            :access_key_id =&gt; ENV['AWS_ACCESS_KEY_ID'],
-            :secret_access_key =&gt; ENV['AWS_SECRET_ACCESS_KEY']
-          },
-          :path =&gt; ":class/:id/:basename_:style.:extension",
-          :url =&gt; ":s3_sg_url"
-      }
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
 
 end
